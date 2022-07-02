7524c53a9ebd698940cf02039c7c1eb1367c2fe1
JuanCarlosGuti <juancgutierrezh@gmail.com>
2022-07-02 18:51:28 -0500

    cambio en proyecto final

--- spring/proyectoFinal/src/main/java/com/dh/proyectoFinal/repository/IdontologoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dh.proyectoFinal.repository;
-
-import com.dh.proyectoFinal.entity.Odontologo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IdontologoRepository extends JpaRepository<Odontologo,Integer> {
-}
--- spring/proyectoFinal/src/main/java/com/dh/proyectoFinal/repository/IturnoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dh.proyectoFinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IturnoRepository extends JpaRepository {
-}
